e8d734534997c027dce02c2199cf906cb8509ef6
AndriyK <andriykutsevol@gmail.com>
2021-12-07 21:51:18 +0200

    fix

--- hw/usb/bus.c
+++ hw/usb/bus.c
@@ -246,12 +246,12 @@ static void usb_qdev_realize(DeviceState *qdev, Error **errp)
 
     USBDevice *dev = USB_DEVICE(qdev);
 
-    char pfname1[] = "/home/dgnet/qemu/dg_qemu_InternalPort";
-    char pfname2[] = ".pcap";
-    char *pfname = (char* )calloc(sizeof(pfname1)+sizeof(pfname2) + 3, sizeof(char));
-    sprintf(pfname, "%s%s%s", pfname1, dev->port_path, pfname2);
+    // char pfname1[] = "/home/dgnet/qemu/dg_qemu_InternalPort";
+    // char pfname2[] = ".pcap";
+    // char *pfname = (char* )calloc(sizeof(pfname1)+sizeof(pfname2) + 3, sizeof(char));
+    // sprintf(pfname, "%s%s%s", pfname1, dev->port_path, pfname2);
 
-    dev->pcap_filename = pfname;
+    // dev->pcap_filename = pfname;
 
     Error *local_err = NULL;
 

4e6c3456def06098094ddaa3da6609bb7bf33366
AndriyK <andriykutsevol@gmail.com>
2021-12-19 22:30:17 +0200

    tracing

--- hw/usb/host-libusb.c
+++ hw/usb/host-libusb.c
@@ -669,7 +669,8 @@ static void usb_host_iso_data_in(USBHostDevice *s, USBPacket *p)
 
     /* copy data to guest */
     xfer = QTAILQ_FIRST(&ring->copy);
-    trace_hw_usb_hostlibC_usb_host_iso_data_in_01_dgtrace(p->iov.size, xfer->xfer->iso_packet_desc[xfer->packet].length);
+    // When broken, some times it is NULL ???
+    //trace_hw_usb_hostlibC_usb_host_iso_data_in_01_dgtrace(p->iov.size, xfer->xfer->iso_packet_desc[xfer->packet].length);
 
     if (xfer != NULL) {
         if (usb_host_iso_data_copy(xfer, p)) {

c220c8bbaae569a4f637256f063fe13cc5870d2e
AndriyK <andriykutsevol@gmail.com>
2021-12-23 17:46:38 +0200

    fix

--- hw/usb/host-libusb.c
+++ hw/usb/host-libusb.c
@@ -1550,28 +1550,28 @@ static void usb_host_handle_control(USBDevice *udev, USBPacket *p,
     USBHostRequest *r;
     int rc;
 
-    trace_usb_host_req_control(s->bus_num, s->addr, p, request, value, index);
+    trace_hw_usb_hostlibC_usb_host_handle_control_0_dgtrace(s->bus_num, s->addr, p, request, value, index);
 
     if (s->dh == NULL) {
         p->status = USB_RET_NODEV;
-        trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
+        //trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
         return;
     }
 
     switch (request) {
     case DeviceOutRequest | USB_REQ_SET_ADDRESS:
         usb_host_set_address(s, value);
-        trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
+        //trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
         return;
 
     case DeviceOutRequest | USB_REQ_SET_CONFIGURATION:
         usb_host_set_config(s, value & 0xff, p);
-        trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
+        //trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
         return;
 
     case InterfaceOutRequest | USB_REQ_SET_INTERFACE:
         usb_host_set_interface(s, index, value, p);
-        trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
+        //trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
         return;
 
     case EndpointOutRequest | USB_REQ_CLEAR_FEATURE:
@@ -1579,7 +1579,7 @@ static void usb_host_handle_control(USBDevice *udev, USBPacket *p,
             int pid = (index & USB_DIR_IN) ? USB_TOKEN_IN : USB_TOKEN_OUT;
             libusb_clear_halt(s->dh, index);
             usb_ep_set_halted(udev, pid, index & 0x0f, 0);
-            trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
+            //trace_usb_host_req_emulated(s->bus_num, s->addr, p, p->status);
             return;
         }
     }
@@ -1606,8 +1606,7 @@ static void usb_host_handle_control(USBDevice *udev, USBPacket *p,
     rc = libusb_submit_transfer(r->xfer);
     if (rc != 0) {
         p->status = USB_RET_NODEV;
-        trace_usb_host_req_complete_4(s->bus_num, s->addr, p,
-                                    p->status, p->actual_length);
+        //trace_usb_host_req_complete_4(s->bus_num, s->addr, p, p->status, p->actual_length);
         if (rc == LIBUSB_ERROR_NO_DEVICE) {
             usb_host_nodev(s);
         }

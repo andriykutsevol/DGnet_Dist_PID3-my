1543735782b7683b01b63b79d604f82b3d097199
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-21 21:12:24 +0200

    с

--- hw/usb/host-libusb.c
+++ hw/usb/host-libusb.c
@@ -1491,10 +1491,15 @@ static void usb_host_handle_control(USBDevice *udev, USBPacket *p,
         r->usb3ep0quirk = true;
     }
 
+    trace_hw_usb_host_libusbC_usb_host_handle_control_0_dgtrace("Before libusb_fill_control_transfer");
+
     libusb_fill_control_transfer(r->xfer, s->dh, r->buffer,
                                  usb_host_req_complete_ctrl, r,
                                  CONTROL_TIMEOUT);
     rc = libusb_submit_transfer(r->xfer);
+
+    trace_hw_usb_host_libusbC_usb_host_handle_control_1_dgtrace("AFTER libusb_fill_control_transfer");
+
     if (rc != 0) {
         p->status = USB_RET_NODEV;
         trace_usb_host_req_complete(s->bus_num, s->addr, p,

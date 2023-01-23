d05cf68888e8f0c171068be8a44681e26d422ced
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-23 20:48:30 +0200

    t

--- hw/usb/desc.c
+++ hw/usb/desc.c
@@ -727,6 +727,11 @@ int usb_desc_get_descriptor(USBDevice *dev, USBPacket *p,
 int usb_desc_handle_control(USBDevice *dev, USBPacket *p,
         int request, int value, int index, int length, uint8_t *data)
 {
+    
+    if (dev->device->bcdUSB)
+        trace_hw_usb_descC_usb_desc_handle_control_0_dgtrace(dev->device->bcdUSB);
+    
+    
     bool msos = (dev->flags & (1 << USB_DEV_FLAG_MSOS_DESC_IN_USE));
     const USBDesc *desc = usb_device_get_usb_desc(dev);
     int ret = -1;
